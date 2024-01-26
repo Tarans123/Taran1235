4a6921511fae991db2891f5b6d76350140117130
taranpreetsingh <taranpreetsingh@10.16.27.131>
2024-01-26 16:43:29 -0500

    added a class

---
+++ src/halftimestudent/java.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halftimestudent;
+
+/**
+ *
+ * @author taranpreetsingh
+ */
+public class java  extends practice{
+    
+    private int numofC;
+
+    public int getNumofC() {
+        return numofC;
+    }
+
+    public java(int numofC) {
+        this.numofC = numofC;
+    }
+
+    public void setNumofC(int numofC) {
+        this.numofC = numofC;
+    }
+    
+    
+    
+}
